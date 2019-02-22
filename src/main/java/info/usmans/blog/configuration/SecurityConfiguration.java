8d270f0cf4f4ffb243263a87e5cb2115eb227a42
Usman Saleem <usman@usmans.info>
2019-02-22 16:30:14 +1000

    Adding springboot security

---
+++ src/main/java/info/usmans/blog/configuration/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package info.usmans.blog.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+        .antMatchers("/rest/blog/**", "/usmansaleem/blog/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin().and()
+        .httpBasic();
+    }
+}
